ab2bd08683eeab13fc4b99bb44968d6284ce9963
Ali <alisoylemez100@gmail.com>
2026-09-02 18:23:06 +0200

    Revert temporary performance instrumentation

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 
-$start = microtime(true);
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
@@ -20,5 +19,3 @@ require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
-
-error_log('TOTAL_LARAVEL_MS=' . round((microtime(true) - $start) * 1000, 2));
