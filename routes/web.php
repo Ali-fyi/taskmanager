ed418a203b93744f072e2e216794b51b10df39d0
Ali <alisoylemez100@gmail.com>
2026-09-02 14:34:40 +0200

    add production performance probe

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,12 @@ Route::get('/', function () {
 Route::get('/probe', function () {
     return response('ok');
 });
+
+Route::get('/perf-probe', function () {
+    $start = microtime(true);
+
+    return response()->json([
+        'message' => 'ok',
+        'server_time_ms' => round((microtime(true) - $start) * 1000, 2),
+    ]);
+});
