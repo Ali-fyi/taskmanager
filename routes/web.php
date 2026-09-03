cb282618d7d3597fd97210f92401076f57aad1d9
Ali <alisoylemez100@gmail.com>
2026-09-03 07:38:14 +0200

    Debug production 500

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
-})->withoutMiddleware([
-    \Illuminate\Session\Middleware\StartSession::class,
-    \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-    \Illuminate\Foundation\Http\Middleware\PreventRequestForgery::class,
-]);
+    return 'Laravel OK';
+});
